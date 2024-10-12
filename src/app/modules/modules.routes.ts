8a307b1c9e4ab5956516ce65e82ea6a2f4b01d5b
santiagoae <santiago_avendano82162@elpoli.edu.co>
2024-10-11 22:05:28 -0500

    feature: update path to users

--- src/app/modules/modules.routes.ts
+++ src/app/modules/modules.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'users',
         loadComponent: () => import('./users-dashboard/users-dashboard.component').then(c => c.UsersDashboardComponent),
         title: 'Users Dashboard'
+    },
+    {
+        path: '**',
+        redirectTo: 'users',
+        pathMatch: 'full'
     }
 ];
